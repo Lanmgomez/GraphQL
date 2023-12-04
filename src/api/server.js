9014a80d24bea59cf86cf4d16a7917dfbe8f77d7
lanmgomez <islan_gomes@hotmail.com>
2023-12-04 16:13:25 -0300

    segundo commit

--- src/api/server.js
+++ src/api/server.js
@@ -2,14 +2,34 @@ import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
 
 const typeDefs = `
+    type User {
+        _id: ID
+        nome: String
+        idade: Int
+        altura: Float
+        cadastro: Boolean
+    }
+
     type Query {
         hello: String
+        user: [User]
     }
 `
 
 const resolvers = {
     Query: {
-        hello: () => "Hello, world!"
+        hello: () => "Hello, world!",
+        user: () => {
+            return [
+                {
+                    _id: () => 1,
+                    nome: () => "Islan",
+                    idade: () => 26,
+                    altura: () => 1.72,
+                    cadastro: () => true
+                }
+            ]
+        }
     }
 }
 
