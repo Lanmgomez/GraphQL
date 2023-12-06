5a8eb1ce611317c58c3d11d01563fb5c6d2a9722
lanmgomez <islan_gomes@hotmail.com>
2023-12-06 11:03:45 -0300

    Enviando atualizacao

--- src/api/server.js
+++ src/api/server.js
@@ -2,14 +2,56 @@ import { ApolloServer } from "@apollo/server"
 import { startStandaloneServer } from "@apollo/server/standalone"
 
 const typeDefs = `
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        active: Boolean!
+    }
+
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        author: User!
+    }
+
     type Query {
-        hello: String
+        users: [User!]!
+        getUserByEmail(email: String!): User!
+    }
+
+    type Mutation {
+        createUser(name: String!, email: String!): User!
     }
 `
+const users = [
+    { _id: String(Math.random()), name: "Islan", email: "islan@hotmail.com", active: true},
+    { _id: String(Math.random()), name: "Vinicius", email: "vini@hotmail.com", active: true},
+    { _id: String(Math.random()), name: "Marcio", email: "marcio@hotmail.com", active: false},
+]
 
 const resolvers = {
+    // Toda Query eh equivalente ao metodo GET
     Query: {
-        hello: () => "Hello, world!"
+        hello: () => "Hello, world!",
+        users: () => users,
+        getUserByEmail: (_, args) => {
+            return users.find((user) => user.email === args.email)
+        }
+    },
+    Mutation: {
+        createUser: (_, args) => {
+            const newUser = {
+                _id: String(Math.random()),
+                name: args.name,
+                email: args.email,
+                active: true
+            }
+
+            users.push(newUser)
+            return newUser
+        }
     }
 }
 
@@ -18,4 +60,3 @@ const server = new ApolloServer({ typeDefs, resolvers })
 const { url } = await startStandaloneServer(server)
 
 console.log(`🚀 Server ready at ${url}`)
-
